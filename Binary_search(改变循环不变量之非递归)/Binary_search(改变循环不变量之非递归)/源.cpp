e427e5a84febcb64e43131d2fcb31882d38b7a14
Youljia <111038084+Youljia@users.noreply.github.com>
2022-09-17 00:01:09 +0800

    Add files via upload

---
+++ Binary_search(改变循环不变量之非递归)/Binary_search(改变循环不变量之非递归)/源.cpp
@@ -0,0 +1,109 @@
+#include<iostream>
+using namespace std;
+
+class Solution
+{
+public:
+	Solution(int* p, int l)
+	{
+		arra = new int[l];
+		this->len = l;
+		for (int i = 0; i < l; ++i)
+			this->arra[i] = p[i];
+		sort();
+	}
+	~Solution()
+	{
+		cout << "This arra is releasing... ..." << endl;
+		if (arra != nullptr)
+			delete[] arra;
+		arra = nullptr;
+		len = NULL;
+	}
+
+	int search(int k)
+	{
+		return binary_search(0, len - 1, k);
+	}
+
+	//非递归调用
+	//在[l,r)里面寻找目标元素
+	int binary_search(int l, int r, int k)
+	{
+		while (l < r)
+		{
+			int mid = l + ((r - l) / 2);
+			if (arra[mid] == k)
+				return mid;
+			else if (arra[mid] > k)
+				r = mid;				//在[l,mid)里面寻找目标元素
+			else
+				l = mid + 1;			//在[mid+1,r)里面寻找目标元素
+		}
+		return -1;
+	}
+
+	void sort()
+	{
+		quick_sort(0, len - 1);
+	}
+	void show()
+	{
+		cout << "[ ";
+		for (int i = 0; i < len; ++i)
+		{
+			cout << arra[i];
+			if (i != len - 1)
+				cout << ",";
+		}
+		cout << " ]" << endl;
+	}
+private:
+	//递归调用
+	//意义:对[l,r]进行排序
+	void quick_sort(int l, int r)
+	{
+		if (l >= r)
+			return;
+		//为区间添加随机性
+		int q = rand() % (r - l + 1);
+		temp(arra[l], arra[l + q]);
+		int p = partition(l, r);
+		quick_sort(l, p - 1);
+		quick_sort(p + 1, r);
+	}
+	int partition(int l, int r)
+	{
+		int j = l;
+		int e = l + 1;
+		for (e; e <= r; ++e)
+		{
+			if (arra[e] < arra[l])
+			{
+				j++;
+				temp(arra[j], arra[e]);
+			}
+		}
+		temp(arra[l], arra[j]);
+		return j;
+	}
+	//互换
+	void temp(int& i, int& j)
+	{
+		int temp = i;
+		i = j;
+		j = temp;
+	}
+private:
+	int* arra;
+	int len;
+};
+
+int main()
+{
+	int p[10] = { 2,3,4,5,6,1,7,8,9,10 };
+	Solution s(p, 10);
+	s.show();
+	cout << s.search(4) << endl;
+	return 0;
+}
